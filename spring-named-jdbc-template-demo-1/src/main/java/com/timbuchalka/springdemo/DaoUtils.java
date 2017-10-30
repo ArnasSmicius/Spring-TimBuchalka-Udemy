c32f7e073849e25e71f776735557786f9d70f7ed
ArnasSmicius <arnasmic@gmail.com>
2017-10-30 20:22:38 +0200

    Building DAO

--- spring-named-jdbc-template-demo-1/src/main/java/com/timbuchalka/springdemo/DaoUtils.java
+++ spring-named-jdbc-template-demo-1/src/main/java/com/timbuchalka/springdemo/DaoUtils.java
@@ -8,31 +8,31 @@ import com.timbuchalka.springdemo.domain.Organization;
 
 public class DaoUtils {
 	
-	public static final String createOperation = "CREATE";
-	public static final String readOperation = "READ";
-	public static final String updateOperation = "UPDATE";
-	public static final String deleteOperation = "DELETE";
-	public static final String cleanupOperation = "TRUNCATE";
+	public final String createOperation = "CREATE";
+	public final String readOperation = "READ";
+	public final String updateOperation = "UPDATE";
+	public final String deleteOperation = "DELETE";
+	public final String cleanupOperation = "TRUNCATE";
 	
-	public static void printOrganizations(List<Organization> orgs, String operation) {
+	public void printOrganizations(List<Organization> orgs, String operation) {
 		System.out.println("\n**************** printing operations after " + operation + " ************");
 		for (Organization org : orgs) {
 			System.out.println(org);
 		}
 	}
 	
-	public static void printOrganization(Organization org, String operation) {
+	public void printOrganization(Organization org, String operation) {
 		System.out.println("\n******Printing organization after invoking " + operation + "*******\n" + org);
 	}
 	
-	public static void printSuccessFailure(String operation, boolean param) {
+	public void printSuccessFailure(String operation, boolean param) {
 		if(param)
 			System.out.println("\nOperation " + operation + " successful");
 		else
 			System.out.println("\nOperation " + operation + " failed");
 	}
 	
-	public static void createSeedData(OrganizationDao dao) {
+	public void createSeedData(OrganizationDao dao) {
 		Organization org1 = new Organization("Amazon", 1994, "65656", 8765, "Work hard, have fun, make history");
 		Organization org2 = new Organization("BMW", 1929, "45454", 5501, "We build ultimate Driving machines");
 		Organization org3 = new Organization("Google", 1996, "57575", 4567, "Don't be evil");
@@ -50,7 +50,7 @@ public class DaoUtils {
 		System.out.println("Created " + createCount + " organizations");
 	}
 	
-	public static void printOrganizationCount(List<Organization> orgs, String operation) {
+	public void printOrganizationCount(List<Organization> orgs, String operation) {
 		System.out.println("\n*********Currently we have " + orgs.size() + " organizations after " + operation + " operation " + "*****************");
 	}
 }
--- spring-named-jdbc-template-demo-1/src/main/java/com/timbuchalka/springdemo/daoimpl/OrganizationDaoImpl.java
+++ spring-named-jdbc-template-demo-1/src/main/java/com/timbuchalka/springdemo/daoimpl/OrganizationDaoImpl.java
@@ -7,6 +7,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
@@ -21,7 +22,7 @@ public class OrganizationDaoImpl implements OrganizationDao {
 
 	@Autowired
 	public void setDataSource(DataSource dataSource) {
-		jdbcTemplate = new JdbcTemplate(dataSource);
+		namedParamJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
 
 	}
 
@@ -37,35 +38,36 @@ public class OrganizationDaoImpl implements OrganizationDao {
 	}
 
 	public Organization getOrganization(Integer id) {
+		SqlParameterSource params = new MapSqlParameterSource("ID", id);
 		String sqlQuery = "SELECT id, company_name, year_of_incorporation, postal_code, employee_count, slogan " +
-				"FROM organization WHERE id = ?";
-		Object[] args = new Object[] {id};
-		Organization org = jdbcTemplate.queryForObject(sqlQuery, args, new OrganizationRowMapper());
+				"FROM organization WHERE id = :ID";
+		
+		Organization org = namedParamJdbcTemplate.queryForObject(sqlQuery, params, new OrganizationRowMapper());
 		return org;
 	}
 
 	public List<Organization> getAllOrganizations() {
 		String sqlQuery = "SELECT * FROM organization";
-		List<Organization> orgList = jdbcTemplate.query(sqlQuery, new OrganizationRowMapper());
+		List<Organization> orgList = namedParamJdbcTemplate.query(sqlQuery, new OrganizationRowMapper());
 		
 		return orgList;
 	}
 
 	public boolean delete(Organization org) {
-		String sqlQuery = "DELETE FROM organization WHERE id = ?";
-		Object[] args = new Object[] { org.getId() };
-		return jdbcTemplate.update(sqlQuery, args) == 1;
+		SqlParameterSource beanParams = new BeanPropertySqlParameterSource(org);
+		String sqlQuery = "DELETE FROM organization WHERE id = :id";
+		return namedParamJdbcTemplate.update(sqlQuery, beanParams) == 1;
 	}
 
 	public boolean update(Organization org) {
-		String sqlQuery = "UPDATE organization SET slogan = ? WHERE id = ?";
-		Object[] args = new Object[] { org.getSlogan(), org.getId() };
-		return jdbcTemplate.update(sqlQuery, args) == 1;
+		SqlParameterSource beanParams = new BeanPropertySqlParameterSource(org);
+		String sqlQuery = "UPDATE organization SET slogan = :slogan WHERE id = :id";
+		return namedParamJdbcTemplate.update(sqlQuery, beanParams) == 1;
 	}
 
 	public void cleanup() {
 		String sqlQuery = "TRUNCATE TABLE organization";
-		jdbcTemplate.execute(sqlQuery);
+		namedParamJdbcTemplate.getJdbcOperations().execute(sqlQuery);
 
 	}
 
