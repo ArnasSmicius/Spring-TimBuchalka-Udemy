98af2b8d8165575cd6acfd3c797b9ca0dcb085fa
ArnasSmicius <arnasmic@gmail.com>
2017-11-02 20:19:43 +0200

    Creating the project infrastructure

---
+++ spring-mvc-java-config-demo/src/main/java/com/timbuchalka/springdemo/service/GenericWelcomeService.java
@@ -0,0 +1,7 @@
+package com.timbuchalka.springdemo.service;
+
+import java.util.List;
+
+public interface GenericWelcomeService {
+	public List<String> getWelcomeMessage(String name);
+}
---
+++ spring-mvc-java-config-demo/src/main/java/com/timbuchalka/springdemo/service/WelcomeService.java
@@ -0,0 +1,18 @@
+ package com.timbuchalka.springdemo.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WelcomeService implements GenericWelcomeService {
+
+		public List<String> getWelcomeMessage(String name) {
+			List<String> myWelcomeMessage = new ArrayList<>();
+			
+			// Add data to the list
+			myWelcomeMessage.add("Hello! ");
+			myWelcomeMessage.add(name);
+			myWelcomeMessage.add(", welcome to the Spring course :-)");
+			
+			return myWelcomeMessage;
+		}
+}
